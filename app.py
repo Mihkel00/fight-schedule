28315ed31a3075bfa04e7c6928bb67ab91c9d2c4
Mihkel00 <mihkel.sylla@gmail.com>
2025-11-20 19:27:47 +0200

    Fix port for Railway

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def home():
     return '<h1>Fight Schedule - Coming Soon!</h1>'
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
