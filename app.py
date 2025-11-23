17113a4bb9865188ac3367ef4340093ccf5d5324
Mihkel00 <mihkel.sylla@gmail.com>
2025-11-23 18:04:54 +0200

    Add HTML template with Tailwind CSS

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Fight Schedule - Day 2!!</h1>'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
