d2fd3a51aeaa95f8b9901b2b4f2c165fbaf5813a
Mihkel00 <mihkel.sylla@gmail.com>
2025-11-23 17:39:39 +0200

    Force rebuild

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Fight Schedule - Coming Soon!</h1>'
+    return '<h1>Fight Schedule - Day 2!!</h1>'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
